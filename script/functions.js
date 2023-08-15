da5d8c5d5f415de1f4a3a744a55c30ad1f890c3c
Heldinnn <124900375+helderjuann@users.noreply.github.com>
2023-08-15 19:16:30 -0300

    Update functions.js

---
+++ script/functions.js
@@ -0,0 +1,29 @@
+$(function() {
+
+    menuMobile();
+
+    function menuMobile() {
+        $('body').on('click','.mobile', function(e) {
+            e.stopPropagation();
+            $(this).find('ul').slideToggle();
+        });
+
+        $('body').on('click', function(e) {
+            e.stopPropagation();
+            $('.mobile').find('ul').slideUp('open');
+        });
+    }
+
+    scrollPage(); 
+
+    function scrollPage() {
+        $('nav a').click(function(){
+            var href = $(this).attr('href');
+            var offSetTop = $(href).offset().top;
+    
+            $('html, body').animate({'scrollTop':offSetTop});
+            return false;
+        });
+    }
+
+})
